40760d99875b1be9e35d1f2b46c61a06d36e2ba4
FlavClover <zaur_from_flava@icloud.com>
2022-11-22 21:15:31 +0300

    Added constant with max receive size of request data

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
+import logging
 
 from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s: %(message)s'
+)
 
 load_dotenv()
 
 
 HOST = os.environ.get('HOST')
 PORT = int(os.environ.get('PORT'))
+RECEIVE_SIZE = int(os.environ.get('RECEIVE_SIZE', 1024))
