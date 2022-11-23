11d7c9b42b8a239001f66b3d51529e134f5c0ccc
FlavClover <zaur_from_flava@icloud.com>
2022-11-23 18:27:13 +0300

    Added 'name' field to logger's messages

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s: %(message)s'
+    format='%(asctime)s - %(name)s - %(levelname)s: %(message)s'
 )
 
 load_dotenv()
